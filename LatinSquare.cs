65397d322db03769a2b93ba74dfcb74e14e24c10
Micah Lapping-Carr <MicahLC@users.noreply.github.com>
2025-12-16 13:53:35 -0600

    Wrote some tests.

---
+++ LatinSquare.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cassidoo_2025_12_15
+{
+	internal class LatinSquare
+	{
+		public static bool ValidLatinSquare(int[][] ls)
+		{
+			return false;
+		}
+
+		public static int[][] GenerateLatinSquare(int n)
+		{
+			return [[]];
+		}
+	}
+}
+
+/**
+ *  [1, 2, 3]
+ *  [2, 3, 1]
+ *  [3, 1, 2]
+ *  
+ *  [1, 2, 3, 4]
+ *  [2, 3, 4, 1]
+ *  [3, 4, 1, 2]
+ *  [4, 1, 2, 3]
+ */
---
+++ LatinSquareTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace cassidoo_2025_12_15
+{
+    [TestClass]
+    public sealed class LatinSquareTests
+    {
+        [TestMethod]
+        public void Input1()
+        {
+            int[][] latinSquare = LatinSquare.GenerateLatinSquare(1);
+            latinSquare[0][0].Should().Equals(1);
+            latinSquare.Should().HaveCount(1);
+            latinSquare[0].Should().HaveCount(1);
+        }
+
+        [TestMethod]
+        public void Input3()
+        {
+			int[][] latinSquare = LatinSquare.GenerateLatinSquare(3);
+            latinSquare.Should().Equal([[1, 2, 3], [2, 3, 1], [3, 1, 2]]);
+		}
+    }
+}
